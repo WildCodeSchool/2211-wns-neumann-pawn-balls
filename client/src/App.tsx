3bc121c0213cd1b73ec0297add7d20fc4dec8020
Hugo Bounichou <hugo.bounichou@atsuke.com>
2023-06-16 12:48:49 +0200

    feat(Cart): added /cart route and context provider to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,33 @@
-import { ToastContainer } from 'react-toastify'
 import { Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 import './App.css'
-import Footer from './pages/Home/components/Footer/Footer'
-import Nav from './pages/Home/components/Nav/Nav'
-import Home from './pages/Home/Home'
+import CartPage from './pages/Cart/CartPage'
 import Dashboard from './pages/Dashboard/Dashboard'
+import Home from './pages/Home/Home'
+import Nav from './pages/Home/components/Nav/Nav'
 import Item from './pages/Item/Item'
+import { AppContext, Context } from './services/context/Context'
+import { fakeCart } from './services/context/FakeData.cart'
 
 function App() {
+  const context: AppContext = {
+    cart: fakeCart,
+  }
   return (
     <>
-      <ToastContainer />
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/products/:id" element={<Item />} />
-      </Routes>
-      <Footer />
+      <Context.Provider value={context}>
+        <ToastContainer />
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/products/:id" element={<Item />} />
+          <Route path="/cart" element={<CartPage />} />
+        </Routes>
+        {
+          //<Footer />
+        }
+      </Context.Provider>
     </>
   )
 }
